db0190add5bacda204afd54d63f92eb3b8e8bbf2
Almat <76092693+ShineRookie@users.noreply.github.com>
2024-12-01 20:30:47 +0500

    add firebase hosting

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBIiAdVT9HXlLy57JsziUX9BzsuoriFDfU",
+    authDomain: "self-portfolio-30176.firebaseapp.com",
+    projectId: "self-portfolio-30176",
+    storageBucket: "self-portfolio-30176.firebasestorage.app",
+    messagingSenderId: "523732969573",
+    appId: "1:523732969573:web:92895a2604d23adfcfbe8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
